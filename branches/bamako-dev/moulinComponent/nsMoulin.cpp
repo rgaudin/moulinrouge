ce8e0767b5bb17e3de354648231aa83d19582376
rgaudin <rgaudin@6d43e8d5-a719-0410-a171-f304b91d6a96>
2007-04-29 16:46:57 +0000

    added support for multiple storage format: no compression, gzip and bzip2

--- branches/bamako-dev/moulinComponent/nsMoulin.cpp
+++ branches/bamako-dev/moulinComponent/nsMoulin.cpp
@@ -18,9 +18,13 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
 
+#include "nsIGenericFactory.h"
+#include "bzlib.h"
+#include "zlib.h"
+#include "nsIMoulin.h"
+
 #include "plstr.h"
 #include "stdio.h"
-#include "nsMoulin.h"
 #include "nsCOMPtr.h"
 #include "nsMemory.h"
 #include "prio.h"
@@ -32,7 +36,18 @@
 #include "nsDirectoryServiceDefs.h"
 #include "nsEmbedString.h"
 
-#include <bzlib.h>#define BZREAD_CHUNK_SIZE 131072
+// 759132FB-D0C5-42B7-B9AD-A7194A2494D3
+#define NS_MOULIN_CID \
+{ 0x759132fb, 0xd0c5, 0x42B7, { 0xb9, 0xad, 0xa7, 0x19, 0x4a, 0x24, 0x94, 0xd3 } }
+#define NS_MOULIN_CONTRACTID "@kunnafoni.org/cpp_nsMoulin;1"
+class nsMoulinImpl : public nsIMoulin
+{
+public:
+    nsMoulinImpl( );
+    virtual ~nsMoulinImpl( );
+    NS_DECL_ISUPPORTS
+    NS_DECL_NSIMOULIN
+};
 
 // c++ constructor
 nsMoulinImpl::nsMoulinImpl( )
@@ -48,9 +63,46 @@ nsMoulinImpl::~nsMoulinImpl( )
 NS_IMPL_ISUPPORTS1_CI(nsMoulinImpl, nsIMoulin);
 
 NS_IMETHODIMP
-nsMoulinImpl::RetrievePageContent(const char *archivefile, PRUint32 startoffset, PRUint32 length, nsACString & _retval)
+nsMoulinImpl::RetrieveGzipContent(const char *archivefile, PRUint32 startoffset, PRUint32 length, nsACString & _retval)
 {
+	int     so = startoffset; //startoffset;
+	int     len = length; //length;
+	int     rem = 0;
+	int     i = 0;
 
+	int nl = so / 131072;
+	rem = so % 131072;
+
+	gzFile infile = gzopen(archivefile, "rb");
+
+	char buffer[131072];
+	char *buffer2 = new char[len];
+	int num_read = 0;
+
+	while ( i <= nl + 1) {
+		if (i < nl) {
+			num_read = gzread(infile, buffer, sizeof(buffer));
+		}
+		if (i == nl + 1) {
+			num_read = gzread(infile, buffer, rem);
+
+			num_read = gzread(infile, buffer2, len);
+		}
+		i++;
+	}
+	gzclose(infile);
+
+	_retval.Append(buffer2, num_read);
+
+   //_retval.Assign(archivefile, strlen(archivefile));
+    
+	return NS_OK;
+}
+
+NS_IMETHODIMP
+
+nsMoulinImpl::RetrieveBzip2Content(const char *archivefile, PRUint32 startoffset, PRUint32 length, nsACString & _retval)
+{
 	FILE*   f;
 	BZFILE* b;
 	int     nBuf;
@@ -60,24 +112,28 @@ nsMoulinImpl::RetrievePageContent(const char *archivefile, PRUint32 startoffset,
 	int     len = length;
 	int     rem = 0;
 	int     i = 0;
-	char    buf2[ len ];
+
+	char *buf2 = new char[len];
 
 	f = fopen ( archivefile, "r" );
 	if ( !f ) {
 		// error ; can't open file
+        _retval.Assign(NS_LITERAL_CSTRING("can not fopen"));
+        return NS_OK;
 	}
-        
+
 	b = BZ2_bzReadOpen ( &bzerror, f, 0, 0, NULL, 0 );
 	if ( bzerror != BZ_OK ) {
 		BZ2_bzReadClose ( &bzerror, b );
-		// error ; bzip can't open file.
+		_retval.Assign(NS_LITERAL_CSTRING("can not BZ2_bzReadOpen"));
+        return NS_OK;
 	}
-        
+
 	int nl = so / 131072;
 	rem = so % 131072;
 
 	bzerror = BZ_OK;
-	while ( bzerror == BZ_OK && i <= nl + 1) {
+	while ( /*bzerror == BZ_OK &&*/ i <= nl + 1) { // needed on darwin. why ??
 		if (i < nl) {
 			nBuf = BZ2_bzRead ( &bzerror, b, buf, 131072 );
 		}
@@ -97,6 +153,40 @@ nsMoulinImpl::RetrievePageContent(const char *archivefile, PRUint32 startoffset,
 	} else {
 		BZ2_bzReadClose ( &bzerror, b );
 	}
-
+    //_retval.Assign(NS_LITERAL_CSTRING("shit"));
 	return NS_OK;
 }
+
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsMoulinImpl)
+static NS_METHOD nsMoulinRegistrationProc(nsIComponentManager *aCompMgr,
+                                          nsIFile *aPath,
+                                          const char *registryLocation,
+                                          const char *componentType,
+                                          const nsModuleComponentInfo *info)
+{
+    return NS_OK;
+}
+static NS_METHOD nsMoulinUnregistrationProc(nsIComponentManager *aCompMgr,
+                                            nsIFile *aPath,
+                                            const char *registryLocation,
+                                            const nsModuleComponentInfo *info)
+{
+    return NS_OK;
+}
+// For each class that wishes to support nsIClassInfo, add a line like this
+NS_DECL_CLASSINFO(nsMoulinImpl)
+static nsModuleComponentInfo components[ ] =
+{
+  { "moulin Component",    // a message to display when component is loaded
+    NS_MOULIN_CID,           // our UUID 
+    NS_MOULIN_CONTRACTID,    // our human readable PROGID or CLSID
+    nsMoulinImplConstructor,
+    nsMoulinRegistrationProc      /* NULL if you dont need one */,
+    nsMoulinUnregistrationProc    /* NULL if you dont need one */,
+    NULL /* no factory destructor */,
+    NS_CI_INTERFACE_GETTER_NAME(nsMoulinImpl),
+    NULL /* no language helper */,
+    &NS_CLASSINFO_NAME(nsMoulinImpl)
+  }
+};
+NS_IMPL_NSGETMODULE(nsMoulinModule, components)
