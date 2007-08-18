7b89d56a444201e506428f0b4438963f9db379c1
rgaudin <rgaudin@6d43e8d5-a719-0410-a171-f304b91d6a96>
2007-08-18 09:54:42 +0000

    multi-lang archives tools

---
+++ branches/bamako-dev/archives/ar/crawler_encyclopedia-portal.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/ruby
+
+STARTTIME = Time.now
+
+# Global defines
+LANG    = "ar"
+RTL     = true
+PROJECT = "encyclopedia-portal"
+MASTER = "encyclopedia"
+NAMESPAC= {:id => 100, :name => :NSPortal}
+UNIQID  = Time.now.to_i
+UNIQFD  = "/var/moulin/#{LANG}/#{PROJECT}_#{UNIQID}"
+MDWKFD  = "/var/www/localhost/htdocs/wiki"
+DBICON  = "DBI:Mysql:#{LANG}_encyclopedia"
+DBIUSER = "ar"
+DBIPASS = "ar"
+BLOCK_SIZE	= 10485760 # 10MB
+
+# 
+LOCALIZED_STRINGS = {
+    :NSCategory => {:raw => "تصنيف", :url => "%D8%AA%D8%B5%D9%86%D9%8A%D9%81"},
+    :NSPortal	=> {:raw => "بوابة", :url => "%D8%A8%D9%88%D8%A7%D8%A8%D8%A9"}
+    }
+
+require '../crawler.rb'
+
+hello()
+
+puts "Duration: #{(Time.now - STARTTIME) /60} mn."
--- branches/bamako-dev/archives/cmd.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$_SERVER['SERVER_NAME'] = "moulinmonk";
-$_SERVER['REMOTE_ADDR'] = "127.0.0.1";
-$_SERVER['PATH_INFO'] = "/" . str_replace("\n","", file_get_contents( $argv[1] ) );
-
-include 'index.php';
-
-?>
