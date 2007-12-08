a462c6e8aa494d53c67abc5d33781efd9329bac1
rgaudin <rgaudin@6d43e8d5-a719-0410-a171-f304b91d6a96>
2007-12-08 19:00:26 +0000

    removed source ; redirecting to Kiwix on SF

--- branches/bamako-dev/autorun/lang_ar.h
+++
@@ -1,6 +0,0 @@
-﻿
-LPCTSTR main_win_title		= _T("مولان - ويكيبيديا بدون اتصال");
-LPCTSTR launch_btn_title	= _T("ابدأ مولان الآن");
-LPCTSTR install_btn_title	= _T("ثبت مولان على القرص الصلب");
-LPCTSTR help_btn_title		= _T("اعرض دليل البدء");
-LPCTSTR quit_btn_title		= _T("أغلق هذه القائمة");
--- trunk/moulin_httpd/version.h
+++
@@ -1,9 +0,0 @@
-/* version.h - version defines for mini_httpd */
-
-#ifndef _VERSION_H_
-#define _VERSION_H_
-
-#define SERVER_SOFTWARE "moulin_httpd"
-#define SERVER_URL "http://code.google.com/p/moulinrouge/"
-
-#endif /* _VERSION_H_ */
