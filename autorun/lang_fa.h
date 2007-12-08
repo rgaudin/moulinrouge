18ceddcf345938fac26d22332269d6018acf63c6
rgaudin <rgaudin@6d43e8d5-a719-0410-a171-f304b91d6a96>
2007-12-08 19:00:26 +0000

    removed source ; redirecting to Kiwix on SF

--- autorun/lang_fa.h
+++
@@ -1,6 +0,0 @@
-﻿
-LPCTSTR main_win_title		= _T("ویکی‌پدیای برون خط");
-LPCTSTR launch_btn_title	= _T("راه انداختن moulin");
-LPCTSTR install_btn_title	= _T("نصب moulin در گرداننده دیسک سخت");
-LPCTSTR help_btn_title		= _T("مشاهده راهنمای آغاز");
-LPCTSTR quit_btn_title		= _T("خروج از گزینگان");
